a5c9bb6c8efa7b25f1e751f3e578d0e8c335e3f9
Akash <sakr1209@gmail.com>
2019-12-22 23:04:47 +0530

    Basic POC implemented

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const express = require('express');
 const app = express();
 const http = require('http');
 
-app.use(express.static('./public'))
+app.use(express.static('./public'));
 
+// app.post('/', (req, res) => {
+//     res.sendFile('login.html')
+// });
+
+app.post('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+  })
+  app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+  })
+  
 
 //To Launch the OpenFin Application we need a manifestUrl.
 const port = 5555;
@@ -15,7 +26,34 @@ const configPath = path.resolve('./public/app.json');
 
 //Start the server server
 //app.use(express.static('./public'));
+
 http.createServer(app).listen(port, async () => {
-    console.log("Server created, starting OpenFin...");
-    openfinLauncher.launch({ manifestUrl: configPath }).catch(err => console.log(err));
+    // console.log("Server created, starting OpenFin...");
+    // openfinLauncher.launch({ manifestUrl: configPath })
+    // .then(console.log(fin.desktop.System.getCookies({name: 'auth'})))
+    // .catch(err => console.log(err));
+
+    try {
+        //Once the server is running we can launch OpenFin and retrieve the port.
+        const port1 = await openfinLauncher.launch({ manifestUrl : configPath });
+
+        //We will use the port to connect from Node to determine when OpenFin exists.
+        const fin = await openfinLauncher.connect({
+            uuid: 'server-connection', //Supply an addressable Id for the connection
+            address: `ws://localhost:${port1}`, //Connect to the given port.
+            nonPersistent: true //We want OpenFin to exit as our application exists.
+        });
+        console.log("Server created, Openfin Start");
+        // fin.desktop.System.getCookies({name: 'auth'}, cookies => {
+        //     cookies.forEach(cookie => {
+        //         console.log(`${cookies.name} ${cookies.expirationDate} ${cookies.content}`);
+        //     });
+        // });
+    } catch (err) {
+        console.error(err);
+    }
+
+
+
+
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,46 @@
-// uncomment line below to register offline cache service worker 
-// navigator.serviceWorker.register('../serviceworker.js');
 
-if (typeof fin !== 'undefined') {
-    init();
-} else {
-    document.querySelector('#of-version').innerText =
-        'The fin API is not available - you are probably running in a browser.';
+function setCookie(){
+    var d = new Date();
+    d.setTime(d.getTime() + (7*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = 'auth = 123345678;' + expires;
 }
 
-//once the DOM has loaded and the OpenFin API is ready
-async function init() {
-    //get a reference to the current Application.
-    const app = await fin.Application.getCurrent();
-    const win = await fin.Window.getCurrent();
-
-    const ofVersion = document.querySelector('#of-version');
-    ofVersion.innerText = await fin.System.getVersion();
-
-    //Only launch new windows from the main window.
-    if (win.identity.name === app.identity.uuid) {
-        //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
-        //for this app we will  launch a child window the first the user clicks on the desktop.
-        app.once('run-requested', async () => {
-            await fin.Window.create({
-                name: 'childWindow',
-                url: location.href,
-                defaultWidth: 320,
-                defaultHeight: 320,
-                autoShow: true
-            });
-        });
-    }
+function destroyCookie(){
+    document.cookie = 'auth = ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; ';
 }
+
+// // uncomment line below to register offline cache service worker 
+// // navigator.serviceWorker.register('../serviceworker.js');
+
+// if (typeof fin !== 'undefined') {
+//     init();
+// } else {
+//     document.querySelector('#of-version').innerText =
+//         'The fin API is not available - you are probably running in a browser.';
+// }
+
+// //once the DOM has loaded and the OpenFin API is ready
+// async function init() {
+//     //get a reference to the current Application.
+//     const app = await fin.Application.getCurrent();
+//     const win = await fin.Window.getCurrent();
+
+//     const ofVersion = document.querySelector('#of-version');
+//     ofVersion.innerText = await fin.System.getVersion();
+
+//     //Only launch new windows from the main window.
+//     if (win.identity.name === app.identity.uuid) {
+//         //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
+//         //for this app we will  launch a child window the first the user clicks on the desktop.
+//         app.once('run-requested', async () => {
+//             await fin.Window.create({
+//                 name: 'childWindow',
+//                 url: location.href,
+//                 defaultWidth: 320,
+//                 defaultHeight: 320,
+//                 autoShow: true
+//             });
+//         });
+//     }
+// }
